aba793bd1116cdefcb12942e9cb31544d51595d6
shashank725 <shashank7rocks@gmail.com>
2021-10-02 11:42:59 +0530

    Added url

--- backend-django/main/urls.py
+++ backend-django/main/urls.py
@@ -3,4 +3,20 @@ from . import views
 
 app_name = 'main'
 urlpatterns = [
-    path('', )
+    path('', views.homepage, name='homepage'),
+    path('login/', views.login, name='login'),
+    path('register/', views.login, name='register'),
+    path('admin_login/', views.login, name='adminlogin'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('staff/dashboard/', views.staffdashboard, name='staffdashboard'),
+    path('dashboard/appointment/', views.appointment, name='appointment'),
+    path('dashboard/appointment/book/', views.book, name='book'),
+    path('staff/dashboard/appointment/', views.staffappointment, name='staffappointment'),
+    path('staff/dashboard/appointment/book', views.staffbook, name='staffbook'),
+    path('dashboard/details', views.details, name='details'),
+    path('staff/dashboard/patient-list/', views.patientlist, name='patientlist'),
+    path('staff/dashboard/doctor', views.doctor, name='doctor'),
+    path('staff/dashboard/housepatient', views.housepatient, name='housepatient'),
+    path('dashboard/bill', views.bill, name='bill'),
+    path('staff/dashboard/bill', views.staffbill, name='staffbill'),
+]
--- backend-django/main/views.py
+++ backend-django/main/views.py
@@ -1,3 +1,59 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+
+def homepage(request):
+    return pass
+
+def login(request):
+    return pass
+
+def register(request):
+    return pass
+
+def stafflogin(request):
+    return pass
+
+def dashboard(request):
+    return pass
+
+def staffdashboard(request):
+    return pass
+
+def appointment(request):
+    return pass
+
+def book(request):
+    return pass
+
+def staffappointment(request):
+    return pass
+
+def staffbook(request):
+    return pass
+
+def details(request):
+    return pass
+
+def patientlist(request):
+     return pass
+
+def doctor(request):
+    return pass
+
+def housepatient(request):
+    return pass
+
+def bill(request):
+    return pass
+
+def staffbill(request):
+    return pass
+
+
+
+
+
+
+
