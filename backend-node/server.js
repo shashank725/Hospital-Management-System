2d137e861262f2e7daed005b73587ca5cb12c0b0
Himanshu Khaitan <himanshukhaitan108@gmail.com>
2021-12-06 22:42:06 +0530

    Updated

--- backend-node/server.js
+++ backend-node/server.js
@@ -5,18 +5,26 @@ const helmet = require("./middlewares/helemt");
 const logging = require("./middlewares/logging");
 const http = require("http");
 const app = express();
+
 app.use(cors);
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(compression());
 app.use(helmet);
 app.use(logging);
 
 // GENERAL ROUTE
+
+/*
+Method  : GET
+Route   : /
+Access  : Public
+*/
 app.get("/", (req, res) => {
   return res.send({
     server: "Care & Cure Hospital",
     status: "online",
-    host: req.headers.host
+    host: req.headers.host,
   });
 });
 
